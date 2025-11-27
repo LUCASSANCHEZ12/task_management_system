ceefae21946596d5de2a510cd21a267c8b92685c
LUCASSANCHEZ12 <lucassanchezsalinas@gmail.com>
2025-11-27 15:53:35 -0400

    feat: enable csrf

--- src/main/java/com/task/manager/demo/security/config/SecurityConfig.java
+++ src/main/java/com/task/manager/demo/security/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
+            //.csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
